8c1c854b1d7295285a4fe04f264bff123a198719
Dom-Filipe <machado.filipe@icloud.com>
2025-06-05 18:23:13 -0300

    Atualiza conteúdo e estilo do site

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,8 +2,7 @@ function Hero() {
   return (
     <section className="hero">
       <div>
-        <h1>Inkverse Studio</h1>
-        <h1>Arte que transcende a pele</h1>
+       
       </div>
     </section>
   );
