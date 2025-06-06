d25753804526d8b022cc38014e9ab0310a85d731
Dom-Filipe <machado.filipe@icloud.com>
2025-06-05 21:25:58 -0300

    Atualiza conteúdo e estilo do site

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,13 @@
 function Hero() {
   return (
-    <section className="hero">
-      <div>
-       
+    <section
+      className="hero bg-black text-white flex items-center justify-center h-screen px-4 bg-cover bg-center relative"
+      style={{ backgroundImage: "url('/assets/111.png')" }}
+    >
+      <div className="absolute inset-0 bg-black bg-opacity-50"></div>
+      <div className="text-center max-w-2xl relative z-10">
+        <h1 className="text-4xl md:text-6xl font-bold mb-4"> </h1>
+        <p className="text-lg md:text-2xl"></p>
       </div>
     </section>
   );
